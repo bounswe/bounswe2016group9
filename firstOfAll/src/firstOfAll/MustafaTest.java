d10c95066d427ac0a191f7842ef7f4e10f7f1379
mustafa <mustafa@mustafa-VirtualBox>
2016-04-27 00:12:29 +0300

    oldubakalım

---
+++ firstOfAll/src/firstOfAll/MustafaTest.java
@@ -0,0 +1,14 @@
+package firstOfAll;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MustafaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
