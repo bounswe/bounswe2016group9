409030e1306da8f13deccadf4a66c86f8095d8d3
Abdulmenaf Kasap <abdulmenaf.kasap@boun.edu.tr>
2016-11-16 21:12:10 +0300

    used main activity as entrance activity #77

--- android/app/src/main/java/com/cmpe451/group9/infograppo/MainActivity.java
+++ android/app/src/main/java/com/cmpe451/group9/infograppo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.cmpe451.group9.infograppo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, NavigationDrawerActivity.class);
+        startActivity(intent);
     }
 }
