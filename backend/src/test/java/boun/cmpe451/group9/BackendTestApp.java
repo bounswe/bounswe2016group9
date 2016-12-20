972a73a5f19e7d42ed49be10f45b05f77d45853b
sehayigitguryol <s.yigitguryol@gmail.com>
2016-12-20 11:51:12 +0300

    Added some backend tests

--- backend/src/test/java/boun/cmpe451/group9/BackendTestApp.java
+++ backend/src/test/java/boun/cmpe451/group9/BackendTestApp.java
@@ -1,8 +1,8 @@
 package boun.cmpe451.group9;
 
-import boun.cmpe451.group9.Models.DB.Topic;
-import boun.cmpe451.group9.Service.Topic.TopicService;
-import boun.cmpe451.group9.Service.User.UserService;
+import boun.cmpe451.group9.Models.DB.*;
+import boun.cmpe451.group9.Service.*;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +24,13 @@ public class BackendTestApp {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private PostService postService;
+
+    @Autowired
+    private CommentService commentService;
+
+
     @Test
     public void deneme(){
         int number = 2;
@@ -39,6 +46,45 @@ public class BackendTestApp {
 
         assertEquals(topicService.getById(t.getEntityId()), t);
     }
+    @Test
+    public void testSaveAndGetPostUnderTopic(){
+        Topic t = new Topic();
+        t.setName("test topic");
+        t.setUser(userService.getById(1));
+        topicService.save(t);
 
+        Post post= new Post();
+        post.setContent("test post");
+        post.setUser(userService.getById(1));
+        post.setTopic(t);
+        postService.save(post);
+
+        assertEquals(topicService.getById(t.getEntityId()), t);
+        assertEquals(postService.getById(post.getEntityId()),post);
+    }
     //TODO Fill this area with additional unit tests
+    @Test
+    public void testSaveComment(){
+        Topic t = new Topic();
+        t.setName("test topic for commenting");
+        t.setUser(userService.getById(1));
+        topicService.save(t);
+
+        Post post= new Post();
+        post.setContent("test post for commenting");
+        post.setUser(userService.getById(1));
+        post.setTopic(t);
+        postService.save(post);
+
+        Comment comment= new Comment();
+        comment.setContet("hello world!");
+        comment.setUser(userService.getById(1));
+        comment.setPostOfComment(post);
+        commentService.save(comment);
+
+        assertEquals(topicService.getById(t.getEntityId()), t);
+        assertEquals(postService.getById(post.getEntityId()),post);
+        assertEquals(commentService.getById(comment.getEntityId()),comment);
+
+    }
 }
