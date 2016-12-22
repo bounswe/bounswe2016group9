f9059970c0bbd189d4922174b48a46a224beffd3
Abdulmenaf Kasap <abdulmenaf.kasap@boun.edu.tr>
2016-12-22 14:19:10 +0300

    created topic activity #162

---
+++ android/app/src/main/java/com/cmpe451/group9/infograppo/activities/TopicActivity.java
@@ -0,0 +1,15 @@
+package com.cmpe451.group9.infograppo.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cmpe451.group9.infograppo.R;
+
+public class TopicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_topic);
+    }
+}
